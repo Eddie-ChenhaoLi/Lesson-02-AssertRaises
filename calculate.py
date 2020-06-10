43597e3e6853a828f938a5e1622a3ce27a0e24a9
lch-howger <360000521@qq.com>
2020-06-10 22:32:12 +0100

    finish lesson 2

--- calculate.py
+++ calculate.py
@@ -1,6 +1,16 @@
-def add(a, b):
-    return a + b
+def add(x, y):
+    return x + y
 
 
-def subtract(a, b):
-    return a - b
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError('Can not divide by zero.')
+    return x / y
--- solutions.py
+++ solutions.py
@@ -8,3 +8,12 @@ class Calculate(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(calculate.subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(calculate.multiply(10, 5), 50)
+
+    def test_divide(self):
+        self.assertEqual(calculate.divide(10, 5), 2)
+
+        # assertRaises
+        self.assertRaises(ValueError, calculate.divide, 10, 0)
